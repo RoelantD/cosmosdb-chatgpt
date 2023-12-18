878592e3ec44b2539a345745634177223f4d3fb5
Roelant Dieben <roelant@icyt.nl>
2023-12-18 21:30:34 +0100

    Add container field

--- Services/CosmosDbService.cs
+++ Services/CosmosDbService.cs
@@ -6,6 +6,8 @@ namespace Cosmos.Chat.GPT.Services;
 
 public class CosmosDbService
 {
+    private readonly Container _container;
+
     public CosmosDbService(string endpoint, string key, string databaseName, string containerName)
     {
         ArgumentNullException.ThrowIfNullOrEmpty(databaseName);
